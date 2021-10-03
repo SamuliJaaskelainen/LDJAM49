6504e96049acc8493cbb7037697dbdfada683768
Samuli <jaaskelainen.samuli@gmail.com>
2021-10-03 18:51:24 +0300

    Bake occlusion culling.

---
+++ LDJAM49/Assets/Scripts/ActivateOnDestory.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateOnDestory : MonoBehaviour
+{
+    public List<GameObject> activateObjets = new List<GameObject>();
+
+    void OnDestroy()
+    {
+        foreach (GameObject go in activateObjets)
+        {
+            go.SetActive(true);
+        }
+    }
+}
